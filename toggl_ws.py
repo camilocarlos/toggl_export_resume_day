21a41328b0fe405a3b37b00e7f4356f833538f14
Camilo Souza <camilocarlos@gmail.com>
2016-06-24 22:07:30 -0300

    Correção de import

--- toggl_ws.py
+++ toggl_ws.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
-from toggl_consume import get_toggl_time_entries
+from toggl_consumer import get_toggl_time_entries
 
 app = Flask(__name__)
 
