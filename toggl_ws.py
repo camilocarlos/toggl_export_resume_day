2341b1d48864d5e46e28d085f1946824cfa966cf
Camilo Souza <camilocarlos@gmail.com>
2016-06-26 15:03:20 -0300

    Habilitação de Cross Origin Resource Sharing - CORS

--- toggl_ws.py
+++ toggl_ws.py
@@ -3,25 +3,19 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask.ext.cors import CORS
 from toggl_consumer import get_toggl_time_entries
 
 app = Flask(__name__)
-
-entries = [
-    {
-        'start' : '1',
-        'stop' : '2',
-        'duration' : '3'
-    }
-]
+cors = CORS(app, resources={r"/toggl_api/*": {"origins": "*"}})
 
 @app.route('/toggl_api/api/v1.0/time_entries', methods=['POST'])
 def get_time_entries():
     api_token =  request.form['api_token']
     start = request.form['start']
     stop = request.form['stop']
-    get_toggl_time_entries(api_token, start, stop)
-    return jsonify({'time_entries': entries})
+    entries = get_toggl_time_entries(api_token, start, stop)
+    return jsonify({'data': entries})
 
 if __name__ == '__main__':
     app.run(debug=True)
